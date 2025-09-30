4279aa72404cec7c60f2257fe9e53a972e81cf36
RylanDS7 <ryds105@gmail.com>
2025-09-30 14:12:19 -0700

    Sim data + improved data plotting

--- UCNspinSim/plotSpinHist.py
+++ UCNspinSim/plotSpinHist.py
@@ -12,7 +12,18 @@ import matplotlib.pyplot as plt
 
 sim = pd.read_csv("UCNspinSim/sim.csv")
 
-bins = np.linspace(0.85, 1, 16)
-print(sim["Ending Spin"])
-plt.hist(sim["Ending Spin"], bins=bins)
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+
+bins = np.linspace(0.75, 1, 51)
+axes[0].hist(sim["Ending Spin"], bins=bins)
+axes[0].set_title("Ending Vertical Spin Componenet (Sz)")
+axes[0].set_xlabel("Sz")
+axes[0].set_ylabel("Number of UCN")
+
+axes[1].scatter(sim["Theta"], sim["Ending Spin"], s=6)
+axes[1].set_title("Ending Vertical Spin Componenet (Sz) vs Initial Path Angle (theta)")
+axes[1].set_xlabel("Theta")
+axes[1].set_ylabel("Sz")
+axes[1].grid()
+
 plt.show()
