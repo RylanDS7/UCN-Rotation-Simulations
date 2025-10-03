955d8619e752ca4cc8637e54ed72ad40267365a3
RylanDS7 <ryds105@gmail.com>
2025-10-03 13:22:26 -0700

    Updated plotting

--- UCNspinSim/plotSpinHist.py
+++ UCNspinSim/plotSpinHist.py
@@ -12,18 +12,24 @@ import matplotlib.pyplot as plt
 
 sim = pd.read_csv("UCNspinSim/sim.csv")
 
+sim["Probability"] = (1 + sim["Ending Spin"])/2
+print(sim["Probability"].mean())
+
 fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 4))
+plt.rcParams.update({'font.size': 14})
 
 bins = np.linspace(0.75, 1, 51)
 axes[0].hist(sim["Ending Spin"], bins=bins)
 axes[0].set_title("Ending Vertical Spin Componenet (Sz)")
-axes[0].set_xlabel("Sz")
-axes[0].set_ylabel("Number of UCN")
+axes[0].set_xlabel("Sz", fontsize=15)
+axes[0].set_ylabel("Number of UCN", fontsize=15)
+axes[0].grid()
 
 axes[1].scatter(sim["Theta"], sim["Ending Spin"], s=6)
-axes[1].set_title("Ending Vertical Spin Componenet (Sz) vs Initial Path Angle (theta)")
-axes[1].set_xlabel("Theta")
-axes[1].set_ylabel("Sz")
+axes[1].set_title("Ending Vertical Spin Componenet (Sz) \nvs Initial Path Angle (theta)")
+axes[1].set_xlabel("Theta", fontsize=15)
+axes[1].set_ylabel("Sz", fontsize=15)
 axes[1].grid()
 
 plt.show()
+
